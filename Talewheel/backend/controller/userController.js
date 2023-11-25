2cbac5cf44474df9533d0ca41e2bbe9d99f5ee13
Shree Varshan G <shreevarshang2003@gmail.com>
2023-11-25 22:39:22 +0530

    changes

--- Talewheel/backend/controller/userController.js
+++ Talewheel/backend/controller/userController.js
@@ -60,7 +60,6 @@ const loginUser = asyncHandler(async (req, res) => {
             _id: user._id,
             username: user.username,
             email: user.email,
-            role: user.role,
             token,
         });
     }
